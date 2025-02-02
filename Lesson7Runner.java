549d43bcca740a848cf416ada37f7c30614372a3
Alice Kryshalovich <alisafaning@gmail.com>
2025-02-02 16:35:35 +0300

    first commit

---
+++ Lesson7Runner.java
@@ -0,0 +1,11 @@
+package Dmdev.CS.lesson7;
+
+public class Lesson7Runner {
+    public static void main(String[] args) {
+        System.out.println(25.7);
+        System.out.println(45L);
+        System.out.println('m');
+        System.out.println("String");
+        System.out.println(true);
+    }
+}
